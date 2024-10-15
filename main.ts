88943c2f0c713180411023226b6a64bde1e51809
yuhangch <i@yuhang.ch>
2024-10-15 18:23:40 +0800

    feat: alpha version.

--- main.ts
+++ main.ts
@@ -1,81 +1,76 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	parseFrontMatterAliases,
+	FrontMatterCache, TFile, CachedMetadata
+} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SuperAliasSettings {
+	keys: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SuperAliasSettings = {
+	keys: 'title'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
+export default class SuperAliasPlugin extends Plugin {
+	settings: SuperAliasSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const injectAliases = (frontmatter: FrontMatterCache) => {
+			if (!frontmatter) return;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			let aliases = parseFrontMatterAliases(frontmatter) ?? [];
+			const extendKeys = this.settings.keys.split(',');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			extendKeys.forEach(key => {
+				const value = frontmatter[key];
+				if (value) {
+					aliases.push(value);
+				}
+			});
+
+			// Remove duplicates
+			frontmatter.aliases = [...new Set(aliases)];
+		};
+
+		const onChangeCallback = (file:TFile,data:string,cache:CachedMetadata) =>
+		{
+			const frontmatter = cache.frontmatter;
+			if (frontmatter) {
+				injectAliases(frontmatter)
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		}
+		this.app.metadataCache.on('changed', onChangeCallback);
+		const onResolveCallback = (file:TFile) =>{
+			const cache = this.app.metadataCache.getFileCache(file);
+			if (cache) {
+				const frontmatter = cache.frontmatter;
+				if (frontmatter) {
+					injectAliases(frontmatter)
 				}
 			}
-		});
+
+		}
+
+		// this.app.parseFrontMatterAliases = parseFrontMatterAliases
+		this.app.metadataCache.on('resolve', onResolveCallback);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SuperAliasTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,26 +86,11 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class SuperAliasTab extends PluginSettingTab {
+	plugin: SuperAliasPlugin;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SuperAliasPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +101,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('keys')
+			.setDesc('Enter keys to be aliased, split by comma')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter keys to be aliased, split by comma')
+				.setValue(this.plugin.settings.keys)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.keys = value;
 					await this.plugin.saveSettings();
 				}));
 	}
